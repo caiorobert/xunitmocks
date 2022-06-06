64f481a841b3314a31d4bc08da09ee0097de5422
Caio Robert Ribeiro Cruz <caio.robert@live.com>
2022-06-05 22:44:24 -0300

    test: commit inicial curso xUnit com mock
    Commit de todo o avanço realizado no curso
    até a aula 04 do curso Mocks em C# da Alura.

---
+++ testes-integracao/src/Alura.CoisasAFazer.Services/Handlers/CommandResult.cs
@@ -0,0 +1,12 @@
+﻿namespace Alura.CoisasAFazer.Services.Handlers
+{
+    public class CommandResult
+    {
+        public bool Success { get; set; }
+        
+        public CommandResult(bool success)
+        {
+            Success = success;
+        }
+    }
+}
---
+++ testes-integracao/tests/Alura.CoisasAFazer.Testes/ObtemCategoriaPorIdExecute.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using Alura.CoisasAFazer.Core.Commands;
+using Alura.CoisasAFazer.Services.Handlers;
+using Moq;
+using Alura.CoisasAFazer.Infrastructure;
+
+namespace Alura.CoisasAFazer.Testes
+{
+    public class ObtemCategoriaPorIdExecute
+    {
+        [Fact]
+        public void QuandoIdForExistenteDeveDeveChamarObtemCategoriaPorIdUmaUnicaVez()
+        {
+            //arrange
+            var idCategoria = 20;
+            var comando = new ObtemCategoriaPorId(idCategoria);
+            var mock = new Mock<IRepositorioTarefas>();
+            var repo = mock.Object;
+            var handler = new ObtemCategoriaPorIdHandler(repo);
+
+            //act
+            handler.Execute(comando);
+
+            //assert
+            mock.Verify(r => r.ObtemCategoriaPorId(idCategoria), Times.Once());
+        }
+    }
+}
