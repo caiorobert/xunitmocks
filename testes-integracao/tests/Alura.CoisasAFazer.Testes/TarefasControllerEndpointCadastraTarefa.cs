18f473afd7d9c816a0da8cddb52192f79bd5230d
Caio Robert Ribeiro Cruz <caio.robert@live.com>
2022-06-08 09:57:48 -0300

    test(TarefasController): teste de controller
    testando o retorno da controller, que ao cadastrar
    uma tarefa, deve retornar status code 200.

---
+++ testes-integracao/tests/Alura.CoisasAFazer.Testes/TarefasControllerEndpointCadastraTarefa.cs
@@ -0,0 +1,44 @@
+﻿using Xunit;
+using Moq;
+using Alura.CoisasAFazer.WebApp.Controllers;
+using Alura.CoisasAFazer.WebApp.Models;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Alura.CoisasAFazer.Services.Handlers;
+using Microsoft.Extensions.Logging;
+using Alura.CoisasAFazer.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Alura.CoisasAFazer.Core.Models;
+
+namespace Alura.CoisasAFazer.Testes
+{
+    public class TarefasControllerEndpointCadastraTarefa
+    {
+        [Fact]
+        public void DadaTarefaComInformacoesValidasDeveRetornar200()
+        {
+            //arrange
+            var mockLogger = new Mock<ILogger<CadastraTarefaHandler>>();
+
+            var options = new DbContextOptionsBuilder<DbTarefasContext>()
+                .UseInMemoryDatabase("DbTarefasContext")
+                .Options;
+            var contexto = new DbTarefasContext(options);
+            var repo = new RepositorioTarefa(contexto);
+            contexto.Categorias.Add(new Categoria(20, "Estudo"));
+            contexto.SaveChanges();
+
+            var controlador = new TarefasController(repo, mockLogger.Object);
+            var model = new CadastraTarefaVM();
+            model.IdCategoria = 20;
+            model.Titulo = "Estudar Xunit";
+            model.Prazo = new DateTime(2022, 12, 31);
+
+            //act
+            var retorno = controlador.EndpointCadastraTarefa(model);
+
+            //assert
+            Assert.IsType<OkResult>(retorno); //200
+        }
+    }
+}
